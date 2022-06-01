3bd096d5c8de357454784afcae3ff69797909a40
CEIBA\alejandro.jaramillo <alejandro.jaramillo@ceiba.com.co>
2022-06-01 18:14:48 -0500

    Cambios pendientes

--- src/main/java/co/proyectoGrado/proyectoGrado/web/config/webConfig.java
+++ src/main/java/co/proyectoGrado/proyectoGrado/web/config/webConfig.java
@@ -9,10 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class webConfig implements WebMvcConfigurer {
 
+    //validar correcta configuraión para el CORS
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200") //debería de ir otro? ¿Cómo colocarlo por variables de ambiente?
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
--- src/main/java/co/proyectoGrado/proyectoGrado/web/controller/DocenteController.java
+++ src/main/java/co/proyectoGrado/proyectoGrado/web/controller/DocenteController.java
@@ -5,12 +5,9 @@ import co.proyectoGrado.proyectoGrado.domain.service.DocenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/teachers")
