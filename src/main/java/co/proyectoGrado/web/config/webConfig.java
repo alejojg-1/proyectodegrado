816a2b6abbb8bd3b64f255aad7b2c5d7f07dfcd8
CEIBA\alejandro.jaramillo <alejandro.jaramillo@ceiba.com.co>
2022-08-08 22:21:07 -0500

    Se organiza origin

--- src/main/java/co/proyectoGrado/web/config/webConfig.java
+++ src/main/java/co/proyectoGrado/web/config/webConfig.java
@@ -1,5 +1,6 @@
 package co.proyectoGrado.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,22 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class webConfig implements WebMvcConfigurer {
 
+    @Value("${allowed.origin}")
+    private String origin;
+
     //validar correcta configuraión para el CORS
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://cygnusft.herokuapp.com") //debería de ir otro? ¿Cómo colocarlo por variables de ambiente?
+                .allowedOrigins(origin)
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") //debería de ir otro? ¿Cómo colocarlo por variables de ambiente?
-                .allowedMethods("*")
-                .maxAge(3600L)
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true);
+
     }
 }
